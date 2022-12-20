122ebd9489e47c8b002268fc86760664434e0d74
Ivan <van.koshkin.fr@gmail.com>
2022-12-20 17:19:10 +0300

    Added blueprint laader

--- loader/views.py
+++ loader/views.py
@@ -1,4 +1,4 @@
-from flask import send_from_directory, Blueprint
+from flask import send_from_directory, Blueprint, render_template
 
 loader_blueprint = Blueprint('loader_blueprint', __name__, template_folder='templates')
 
@@ -6,3 +6,15 @@ loader_blueprint = Blueprint('loader_blueprint', __name__, template_folder='temp
 @loader_blueprint.route("/uploads/<path:path>")
 def static_dir(path):
     return send_from_directory("./uploads", path)
+
+
+@loader_blueprint.route("/post/", methods=['GET'])
+def post_page():
+    return render_template("post_form.html")
+
+
+@loader_blueprint.route("/post/", methods=['POST'])
+def upload_page():
+    return render_template("post_uploaded.html")
+
+
